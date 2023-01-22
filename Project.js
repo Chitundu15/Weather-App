264bc97c2af6a0cdd1c6274757a5f6b96f25f2c7
Chitundu15 <milimbochitundu15@gmail.com>
2023-01-22 14:39:33 +0200

    Added icon through JavaScript

--- Project.js
+++ Project.js
@@ -1,6 +1,7 @@
 let now = new Date();
 let apiKey = "2b6fdad0cbd018949c50c70f72250726";
 
+
 function displayDateAndTime(event) {
 event.preventDefault();
 let date = document.querySelector("#first-day");
@@ -20,10 +21,14 @@ if (minutes < 10) {
 let time = document.querySelector("#search-button");
 time.addEventListener("submit", displayDateAndTime);
 
-//Current Weather
+
 function displayCity(response) {
 let temperature = Math.round(response.data.main.temp);
 let city = response.data.name;
+let mainIcon  = document.querySelector("#main-icon");
+mainIcon.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+let alternativeText = document.querySelector("#main-icon");
+alternativeText .setAttribute("alt",response.data.weather[0].description);
 let temp = document.querySelector("#temperature");
 temp.innerHTML = temperature;
 let humidity = document.querySelector("#humid");
@@ -40,7 +45,7 @@ let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "S
 let today = days[now.getDay()];
 let hours = now.getHours();
 let minutes = now.getMinutes();
-date.innerHTML = `${today}`;
+date.innerHTML = `Last Updated: ${today}`;
 if (minutes < 10) {
   time.innerHTML = `${hours}:0${minutes}`;
 } else {
@@ -52,7 +57,7 @@ if (hours < 10) {
   time.innerHTML = `${hours}:${minutes}`;
 }
 }
-
+//Search weather
 function handleSubmit(event) {
 event.preventDefault();
 let input = document.querySelector("#search-input");
@@ -63,7 +68,7 @@ axios.get(`${apiUrl}&appid=${apiKey}`).then(displayCity);
 let city = document.querySelector("#city-form");
 city.addEventListener("submit", handleSubmit);
 
-
+//Current Weather
 function showLocation(position) {
 let lat = position.coords.latitude;
 let long = position.coords.longitude;
@@ -76,3 +81,32 @@ navigator.geolocation.getCurrentPosition(showLocation);
 }
 let button = document.querySelector("#current-button");
 button.addEventListener("click", showMyPosition);
+
+// Default City
+function defaultCity(){
+  let city="Lusaka";
+  apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(`${apiUrl}&appid=${apiKey}`).then(displayDefaultCity);
+}
+
+function displayDefaultCity(response){
+  console.log(response);
+  let city = document.querySelector("#main-city");
+  city.innerHTML = response.data.main.name;
+  let icon=document.querySelector("main-icon");
+  icon.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  let alternativeText = document.querySelector("#main-icon");
+  alternativeText .setAttribute("alt",response.data.weather[0].description);
+  let date = document.querySelector("#first-day");
+  let time = document.querySelector("#time");
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  let today = days[now.getDay()];
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  date.innerHTML = `${today}`;
+  if (minutes < 10) {
+  time.innerHTML = `${hours}:0${minutes}`;
+  } else {
+  time.innerHTML = `${hours}:${minutes}`;
+  }
+}
